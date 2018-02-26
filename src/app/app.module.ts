1e44077b9a1864dd55c5e26ac357b6150a845c68
matias <matiasbp7@hotmail.com>
2018-02-26 12:24:33 -0300

    componente contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ContctoComponent } from './contcto/contcto.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContctoComponent
   ],
   imports: [
     BrowserModule
