1a93ba6e725eedde2ad8e93cb1997ff30b03fc46
ezegallo706 <ezequiel.r.gallo@gmail.com>
2018-02-26 12:27:59 -0300

    creado componente productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './productos/productos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule
