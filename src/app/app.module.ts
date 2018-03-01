7601a710267d8c4d108469cc5d05d8a20cf2ab0f
matias <matiasbp7@hotmail.com>
2018-03-01 12:07:09 -0300

    maqueta casi terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,10 @@ import { AppComponent } from './app.component';
 import { ContctoComponent } from './contcto/contcto.component';
 
 import { HomeComponent } from './home/home.component';
-
+  
 import { MitrabajoComponent } from './mitrabajo/mitrabajo.component';
-
-
-import { HomeComponent } from './home/home.component';
-
-import { MitrabajoComponent } from './mitrabajo/mitrabajo.component';
-
 
 import { ProductosComponent } from './productos/productos.component';
-
-
-import { HomeComponent } from './home/home.component';
-
-import { MitrabajoComponent } from './mitrabajo/mitrabajo.component';
 
 
 
@@ -29,7 +18,7 @@ import { MitrabajoComponent } from './mitrabajo/mitrabajo.component';
   declarations: [
     AppComponent,
 
-    ProductosComponent
+    ProductosComponent,
 
 
     HomeComponent,
