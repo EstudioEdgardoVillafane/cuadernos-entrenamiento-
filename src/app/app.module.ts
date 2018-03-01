e18e3ba014e75ea311833b0c0a12063eb40f5134
eze <ezequiel.r.gallo@gmail.com>
2018-03-01 17:56:18 -0300

    creacion de carrusel en componente productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 //import{} from '@'
 import { AppRoutingModule } from './app-routing/app-routing.module';
@@ -17,16 +18,10 @@ import { MitrabajoComponent } from './mitrabajo/mitrabajo.component';
 import { ProductosComponent } from './productos/productos.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 
-import { AppComponent } from './app.component';
-import { ContctoComponent } from './contcto/contcto.component';
 
-import { ProductosComponent } from './productos/productos.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { MitrabajoComponent } from './mitrabajo/mitrabajo.component';
 import { FooterComponent } from './footer/footer.component';
 
 
@@ -35,13 +30,7 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-
     ProductosComponent,
-
-
-
-
-
     HomeComponent,
     ContctoComponent,
     MitrabajoComponent,
@@ -50,11 +39,10 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-
+   
     //NgbModule.forRoot (),
     AppRoutingModule,
-    FormsModule
-
+    FormsModule,
     NgbModule,
     CarouselModule.forRoot()
 
