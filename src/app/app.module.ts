ceb8b65bf252ce19757496bb27eed64c3fa92034
CristianHourcade <hourcadecristian@gmail.com>
2018-03-15 13:16:07 -0300

    Trabajo toto finish.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { FormsModule }    from '@angular/forms';
-
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-
-
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { FormsModule }    from '@angular/forms';
-
-//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule }    from '@angular/common/http';
@@ -22,22 +11,12 @@ import { HttpClientModule }    from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ContctoComponent } from './contcto/contcto.component';
 import { ProductosComponent } from './productos/productos.component';
-
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MitrabajoComponent } from './mitrabajo/mitrabajo.component';
 import { FooterComponent } from './footer/footer.component';
-
 import { CuadernoTotoService } from '../app/cuaderno-toto.service';
 import { BackendComponent } from './backend/backend.component';
-
-import { ProductosComponent } from './productos/productos.component';
-import { AppComponent } from './app.component';
-import { MitrabajoComponent } from './mitrabajo/mitrabajo.component';
-import { HomeComponent } from './home/home.component';
-import { ContctoComponent } from './contcto/contcto.component';
-
-
-
-
 
 @NgModule({
   declarations: [
@@ -52,30 +31,14 @@ import { ContctoComponent } from './contcto/contcto.component';
   ],
   imports: [
     BrowserModule,
-
     HttpModule,
     AppRoutingModule,
     FormsModule,
     CarouselModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    
  ],
   providers: [CuadernoTotoService],       //IMPORTAR LOS SERVICIOS
-
-   
-    //NgbModule.forRoot (),
-    AppRoutingModule,
-    FormsModule,
-
-
-   // NgbModule,
-
-  // NgbModule,
-
-   CarouselModule.forRoot()
-
-  ],
-  providers: [],
-
   bootstrap: [AppComponent]
 })
 export class AppModule { }
