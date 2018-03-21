b7e6c6767f385f120216c5ca2fde4790da731382
CristianHourcade <hourcadecristian@gmail.com>
2018-03-21 14:34:32 -0300

    script probando.php

--- src/php/probando.php
+++ src/php/probando.php
@@ -1,6 +1,6 @@
 <?php
-include '../../src/php/cuaderno.php';
-include '../../src/php/connect.php';
+require_once 'cuaderno.php';
+require_once 'connect.php';
 
 $NewConnect = new Cuaderno();
 	
@@ -11,17 +11,18 @@ $NewConnect = new Cuaderno();
 	
 	$uploadedfileload="true";
 	$uploadedfile_size=$_FILES['uploadedfile']['size'];
-	$file_name=$_FILES['uploadedfile']['name'];
+	$numero_aleatorio = rand(1,10);
+	$file_name = $_FILES['uploadedfile']['name'];
+	// $file_name = $numero_aleatorio.$file_name;
 	$add='uploads/'.$file_name;
 	
 	move_uploaded_file ($_FILES['uploadedfile']['tmp_name'], $add);
 	
 		echo " Ha sido subido satisfactoriamente";
-		$URL="http://localhost/cuad/cuadernos/dist/php/uploads/".$_FILES['uploadedfile']['name'];
+		$URL="php/uploads/".$file_name;
         $sql = "INSERT INTO producto (nombre,descripcion,precio,categoria,foto,status) 
 		VALUES ('".$Nombre."','".$Descripcion."','".$Precio."','".$Categoria."','".$URL."','1')";
         $NewConnect->ExecuteSql($sql);
 	
 
-
-?>
+ 
